8dcd937cf8cac2512a58a09481e40911ca174177
Juan Pablo Caponetto <pablito__cs@hotmail.com>
2023-08-08 18:32:31 -0300

    Modificando Readme y Navbar para entrega final

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ function NavBar() {
   return (
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid ">
-              <Link className="navbar-brand" to={"/"}>Maquinort</Link>
+              <Link className="navbar-brand menu-link" to={"/"}>Maquinort</Link>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
