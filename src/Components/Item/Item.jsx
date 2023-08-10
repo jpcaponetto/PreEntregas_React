235ae861784f4d567e7ac41d4c8b836b33120b7b
Juan Pablo Caponetto <pablito__cs@hotmail.com>
2023-08-10 20:48:49 -0300

    quitando codigo comentado

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-// import Button from 'react-bootstrap/Button';
-// import Card from 'react-bootstrap/Card';
 
 
 
@@ -17,41 +15,7 @@ function Item({productos}) {
         <Link to={`/item/${id}`} className="ver-mas">Ver mas</Link>
       </div>
     </div>
-    // <Card style={{ width: '18rem' }}>
-    //   <Card.Img variant="top" src={imagen} />
-    //   <Card.Body>
-    //     <Card.Title> {titulo} </Card.Title>
-    //     <Card.Text>
-    //      Categoria: {categoria}
-    //     </Card.Text>
-    //     <Card.Text>
-    //       Precio: ${precio}
-    //     </Card.Text>
-    //     <Button variant="danger" href={`/item/${id}`} >Ver más</Button>
-    //   </Card.Body>
-    // </Card>
   );
 }
 
 export default Item;
-
-
-
-// const Item = ( {productos} ) => {
-//   return (
-
-//     <div className="producto">
-//         <img src={productos.imagen} alt='img' />
-//         <div>
-//             <p>{productos.titulo}</p>
-//             <p>  <strong>Precio: ${productos.precio} </strong></p>
-//             <p>  Categoria: {productos.categoria.nombre} </p>
-            
-//         </div>
-//     </div>
-
-//   )
-
-// }
-
-// export default Item
