6e7522cd3df5ebb66d7e26b02d61c922743864c2
Juan Pablo Caponetto <pablito__cs@hotmail.com>
2023-08-12 15:50:38 -0300

    Proyecto Final

--- src/Components/Error404/Error404.jsx
+++ src/Components/Error404/Error404.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Error404 = () => {
   return (
-    <div><h1>Error 404 - Pagina no encontrada</h1></div>
+    <div><h1>Error 404 - Page Not Found </h1></div>
   )
 }
 
