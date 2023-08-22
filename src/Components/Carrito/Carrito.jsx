3fcabaf71dde3d6223e80a697ea3f8afe53a9423
Juan Pablo Caponetto <pablito__cs@hotmail.com>
2023-08-22 19:12:17 -0300

    correciones para re entrega

--- src/Components/Carrito/Carrito.jsx
+++ src/Components/Carrito/Carrito.jsx
@@ -13,12 +13,14 @@ const Carrito = () => {
   return (
     
     <div className='container'>
-      <h1 className='main-title'>Carrito</h1>
+      <h1 className='main-title text-center'>Carrito</h1>
       {
         carrito.map((prod) => (
           <div key={prod.id}>
+            <div className='container card' style={{ width: '18rem' }}>
+              <div className='row card-body'>
           <h3> {prod.titulo} </h3>
-          <img src={prod.imagen} alt={prod.titulo} />
+          <img src={prod.imagen} alt={prod.titulo} className='card-img-top' />
           <p>Precio Unitario: ${prod.precio} </p>
           <p>Precio Total: ${prod.precio * prod.cantidad}</p>
           <p>Cant: {prod.cantidad}</p>
@@ -27,19 +29,18 @@ const Carrito = () => {
             Eliminar producto del carrito 
           </button>
           </div>
-
+          </div>
+          </div>
         
-
-
         ))
       } 
       
       {
         carrito.length > 0 ?  
         <>
-      <h2>Total neto: ${precioTotal()}</h2>
-      <button className='agregar-al-carrito' onClick={handleVaciar}>Vaciar</button>
-       <Link to="/checkout" className='agregar-al-carrito'>Finalizar compra </Link>
+      <h2 className='text-center'>Total neto: ${precioTotal()}</h2>
+      <button className='agregar-al-carrito justify-content-center' onClick={handleVaciar}>Vaciar</button>
+       <Link to="/checkout" className='agregar-al-carrito text-center'>Finalizar compra </Link>
         </> :
             <h2>El Carrito está vacio</h2>
       }
